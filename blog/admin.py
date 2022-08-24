3652bc292dcae6adf225451c8185a1a6d5a1bd8a
mahtab <mahtabranjbar93@gmail.com>
2022-08-24 13:15:01 +0430

    Add ArticleAdmin

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
 
-# Register your models here.
+from blog.models import Article
+
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug', 'published_at', 'status']
+    list_filter = ['published_at', 'status']
+    search_fields = ['title', 'description']
+    prepopulated_fields = {'slug': ('title',)}
+    ordering = ['-status', '-published_at']
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Article(models.Model):
+    STATUS_CHOICES = (
+        ('d', 'Draft'),
+        ('p', 'Published')
+    )
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True, )
+    description = models.TextField()
+    thumbnail = models.ImageField(upload_to='images')
+    published_at = models.DateTimeField(default=timezone.now)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=200, choices=STATUS_CHOICES)
+
+    def __str__(self):
+        return self.title
