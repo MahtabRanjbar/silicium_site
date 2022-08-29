a599201407f0afab44074910f91392fd02290453
mahtab <mahtabranjbar93@gmail.com>
2022-08-29 10:56:29 +0430

    fix problem in admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -12,7 +12,7 @@ class ArticleAdmin(admin.ModelAdmin):
     ordering = ['-status', '-published_at']
 
     def category_to_str(self, obj):
-        return ", ".join([category.title for category in obj.category.all()])
+        return ", ".join([category.title for category in obj.category.published()])
     category_to_str.short_description = 'categories'
 
 
