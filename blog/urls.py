8b102ea2108011f1c7dc79f4b85187dc4c419fe5
mahtab <mahtabranjbar93@gmail.com>
2022-08-29 11:13:45 +0430

    Add pagination to category

--- blog/urls.py
+++ blog/urls.py
@@ -5,8 +5,9 @@ from blog import views
 app_name = 'blog'
 urlpatterns = [
     path('', views.home, name='home'),
-    path('article/<slug:slug>', views.detail_view, name='detail'),
-    path('category/<slug:slug>', views.category_view, name='category'),
     path('page/<int:page>', views.home, name='home'),
+    path('article/<slug:slug>', views.detail_view, name='detail'),
+    path('category/<slug:slug>', views.category_view, name='category'),   
+    path('category/<slug:slug>/page/<int:page>', views.category_view, name='category'),
     
 ]
--- blog/views.py
+++ blog/views.py
@@ -6,8 +6,8 @@ from blog.models import Article, Category
 # Create your views here.
 def home(request, page=1):
     article_list = Article.objects.published()
-    categories = Category.objects.filter(status=True)   
-    
+    categories = Category.objects.filter(status=True)
+
     paginator = Paginator(article_list, 2)
     articles = paginator.get_page(page)
     context = {'articles': articles,
@@ -19,9 +19,13 @@ def detail_view(request, slug):
     query = get_object_or_404(Article, slug=slug)
     context = {'article': query}
     return render(request, 'blog/detail.html', context=context)
-    
 
-def category_view(request, slug):
-    context = {'categories': get_object_or_404(Category, slug=slug)}
+
+def category_view(request, slug, page=1):
+    category = get_object_or_404(Category, slug=slug)
+    articles_list = category.article.published()
+    paginator = Paginator(articles_list, 2)
+    articles = paginator.get_page(page)
+    context = {'category': category,
+               'articles': articles}
     return render(request, 'blog/category.html', context)
-    
