f1f03551e9f15352152c356c1733215fe6baa032
mahtab <mahtabranjbar93@gmail.com>
2022-08-29 11:22:22 +0430

    Add parent field to Category model

--- blog/models.py
+++ blog/models.py
@@ -14,6 +14,7 @@ class CategoryManager(models.Manager):
 
 
 class Category(models.Model):
+    parent = models.ForeignKey('self', default=None, null=True, blank=True, on_delete=models.SET_NULL, related_name='children', verbose_name='subcateogry')
     title = models.CharField(max_length=200)
     slug = models.SlugField(max_length=200, unique=True, )
     status = models.BooleanField(max_length=200, verbose_name='Be visible')
