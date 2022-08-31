b0aa2d522c8d91596879f91f6040e96febbd1c80
mahtab <mahtabranjbar93@gmail.com>
2022-08-31 07:52:16 +0430

    add actions to ArticleAdmin

--- blog/admin.py
+++ blog/admin.py
@@ -10,6 +10,25 @@ class ArticleAdmin(admin.ModelAdmin):
     search_fields = ['title', 'description']
     prepopulated_fields = {'slug': ('title',)}
     ordering = ['-status', '-published_at']
+    actions = ['make_draft', 'make_published']
+
+    @admin.action(description='Mark selected stories as draft')
+    def make_draft(self, request, queryset):
+        row_updated = queryset.update(status="d")
+        if row_updated == 1:
+            message = '1 sotry was'
+        else:
+            message = f"{row_updated} story were"
+        self.message_user(request, f"{message} successfully marked as draft")
+
+    @admin.action(description='Mark selected stories as published')
+    def make_published(self, request, queryset):
+        row_updated = queryset.update(status="p")
+        if row_updated == 1:
+            message = '1 sotry was'
+        else:
+            message = f"{row_updated} story were"
+        self.message_user(request, f"{message} successfully marked as published")
 
     def category_to_str(self, obj):
         return ", ".join([category.title for category in obj.category.published()])
