b4c8f6a4b1e64954308cc510b487a86a5fea9869
mahtab <mahtabranjbar93@gmail.com>
2022-08-31 09:03:36 +0430

    fix minor bug

--- config/urls.py
+++ config/urls.py
@@ -21,7 +21,7 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path ('', include('blog.urls')),
-    path ('', include('acoounts.urls')),
+    path ('', include('accounts.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
