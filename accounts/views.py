4ce54d4e1b097bd2791092710b2c566fd3ae6275
mahtab <mahtabranjbar93@gmail.com>
2022-09-01 17:08:57 +0430

    add adminlte templates

--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,12 @@
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
 from django.views.generic import ListView
-from django.contrib.auth.decorators import login_required
 
 
 @login_required
 def home(request):
-    return render(request, 'registration/home.html')
+    return render(request, 'registration/adminlte/base.html')
 
 # Create your views here.
 class HomeView(LoginRequiredMixin, ListView):
