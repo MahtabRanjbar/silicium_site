4f921eb259aa0033fde386a06729d376d4f2dec0
mahtab <mahtabranjbar93@gmail.com>
2022-09-10 10:47:06 +0430

    add Change Password Base

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,6 +6,8 @@ app_name = 'accounts'
 urlpatterns = [
     path('login', views.Login.as_view(), name='login'),
     path('logout', auth_views.LogoutView.as_view(), name='logout'),
+    path('password_change/', auth_views.PasswordChangeView.as_view(template_name='accounts/registration/password_change.html'), name='password-change'),
+    path('password_change/done', auth_views.PasswordChangeDoneView.as_view(template_name='accounts/registration/password_change_done.html'), name='password-change-done'),
     path('', views.ArticleList.as_view(), name='home'),
     path('article/create', views.ArticleCreate.as_view(), name='article-create'),
     path('article/update/<int:pk>', views.ArticleUpdate.as_view(), name='article-update'),
